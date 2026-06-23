0811dbbdc657558d7d341fabca2231e871c409fd
Ricardo Jesus O. Lopez <ricardojesusorracalopez@gmail.com>
2026-06-23 04:31:14 -0400

    feat: create timestamp api

--- index.js
+++ index.js
@@ -20,8 +20,36 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get('/api/:date?', (req, res) => {
+  const dateParam = req.params.date;
+  let date;
+
+
+  if (!dateParam) {
+    date = new Date();
+  } else {
+    
+    const timestamp = parseInt(dateParam);
+
+    if (!isNaN(timestamp) && dateParam.length === 13) {
+      date = new Date(timestamp);
+    } else {
+    
+      date = new Date(dateParam);
+    }
+  }
+
+
+  if (date.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
